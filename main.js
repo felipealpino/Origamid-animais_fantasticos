d5878bdd134c57d6153cad8b76039e61d6117285
Felipe Alpino <felipealpino1@gmail.com>
2020-12-09 16:26:28 -0300

    Finalizando js projeto

--- main.js
+++ main.js
@@ -1,53 +1,79 @@
-//1
-const img = document.querySelectorAll('img');
-console.log(img);
 
-//2
-const imgret = document.querySelectorAll('img[src^="img/image"]');
-console.log(imgret);
+function initTabFunc(){
+    const animaisLista = document.querySelectorAll('.animais-lista li')
+    const animaisDescricao = document.querySelectorAll('.animais-descricao.animais section')
+    const htmlTodo = document.documentElement
 
-//3
-var links = document.querySelectorAll('a[href^="#"]');
-console.log(links);
+    animaisDescricao[0].classList.add('ativo-section')
+    htmlTodo.className += ' js'
 
-//4
-const agah2 = document.querySelector('.animais-descricao h2');
-console.log(agah2);
-
-//5
-const lastp = document.querySelectorAll('p');
-lastp.forEach(function(item, index){
-    ult = index;
-});
-console.log(lastp[ult]);
-
-
-//1.1
-const paragrafos = document.querySelectorAll('p');
-paragrafos.forEach((item) => {
-    console.log(item);
-});
-
-//2.1
-paragrafos.forEach((item) => {
-    console.log(item.innerHTML);
-});
-
-//3.1
-const imgs = document.querySelectorAll('img');
-imgs.forEach((item,index) => {
-    console.log(item,index);
-});
-
-//4.1
-let i=0;
-imgs.forEach(() => {
-    console.log(i++);
-});
-
-//5.1
-i = 0;
-imgs.forEach(() => console.log(i++));
+    animaisLista.forEach((foto,index) => {
+        foto.addEventListener('click', ()=> {
+            addAtivo(index)        
+        });
+    });
+    function addAtivo(index) {
+        animaisDescricao.forEach((section) => {
+            section.classList.remove('ativo-section')
+        })
+        animaisDescricao[index].classList.add('ativo-section')
+    }
+}
+initTabFunc();
 
 
+function initAcordion(){
+    const faqListaDt = document.querySelectorAll('.faq-lista dt')
+    faqListaDt[0].classList.add('inverteSeta')
+    faqListaDt[0].nextElementSibling.classList.add('ativo-faq')
+    
+    function ativaDD(){
+        this.classList.toggle('inverteSeta')
+        this.nextElementSibling.classList.toggle('ativo-faq')
+    }
+    faqListaDt.forEach((dt) => {
+        dt.addEventListener('click', ativaDD )
+    }   )
+}
+initAcordion();
 
+
+function initScrollSuave(){
+    const linksInternos = document.querySelectorAll('a[href^="#"]')
+
+    function scrollToSection(event){
+        event.preventDefault()
+        const href = event.currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+        console.log(section)
+        section.scrollIntoView({
+            behavior:'smooth',
+            block:'start'
+        })
+    }
+
+    linksInternos.forEach((item) => {
+        item.addEventListener('click', scrollToSection)
+    })
+}
+initScrollSuave();
+
+
+function initAnimacaoScroll(){
+    const sections = document.querySelectorAll('.js-scroll')
+    const metadeJanela = window.innerHeight * 0.7
+    animaScroll()
+
+    function animaScroll(){
+        sections.forEach((section) => {
+            const distSectionTop = section.getBoundingClientRect().top
+            const isSectionVisible = distSectionTop - metadeJanela
+            if (isSectionVisible < 0) {
+                section.classList.add('ativo')
+            }
+        })
+    }
+    window.addEventListener('scroll', animaScroll)
+}
+
+initAnimacaoScroll()
